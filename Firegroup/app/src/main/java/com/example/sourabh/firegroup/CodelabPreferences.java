572f2a0ad352534cb8cd7eea779eee27612f3616
saurav12994 <saurav12994@gmail.com>
2017-06-21 16:21:45 +0530

    first commit

---
+++ Firegroup/app/src/main/java/com/example/sourabh/firegroup/CodelabPreferences.java
@@ -0,0 +1,11 @@
+package com.example.sourabh.firegroup;
+
+/**
+ * Created by Sourabh on 21-06-2017.
+ */
+public class CodelabPreferences {
+
+    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
---
+++ Firegroup/app/src/main/java/com/example/sourabh/firegroup/MyFirebaseMessagingService.java
@@ -0,0 +1,22 @@
+package com.example.sourabh.firegroup;
+
+/**
+ * Created by Sourabh on 21-06-2017.
+ */
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFMService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " + remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+    }
+}
